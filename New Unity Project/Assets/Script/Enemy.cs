21de56f5c707352728bf3468c4c9071a74feeba4
Ossj400 <Ossj400@gmail.com>
2019-02-24 20:14:28 +0100

    1 raz :P

---
+++ New Unity Project/Assets/Script/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum State
+    {
+        LOOKFOR,
+        GOTO,
+        ATTACK
+    }
+    public State curState;
+    public float speed = .5f;
+    public float goToDistance = 13;
+    public float attackDistance = 4;
+    public Transform target;
+    public string PlayerTag = "Player";
+
+
+    // Start is called before the first frame update
+    IEnumerator  Start()
+    {
+        while (true)
+        {
+            switch(curState)
+            {
+                case State.LOOKFOR:
+                    LookFor();
+                    break;
+                case State.GOTO:
+                    GoTo();
+                    break;
+                case State.ATTACK:
+                    Attack();
+                    break;
+            }
+            yield return 0;
+        }
+    }
+
+    void LookFor()
+    {
+
+        print("Hi we are in lookfor");
+
+        if (Vector3.Distance(target.position, transform.position)< goToDistance)
+        {
+            curState = State.GOTO;
+        }
+
+    }
+
+
+    void GoTo()
+    {
+        print("Hi we are in goto");
+    }
+
+
+    void Attack()
+    {
+
+    }
+
+
+}
---
+++ New Unity Project/Assets/Script/Player.cs
@@ -0,0 +1,89 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10;
+    public float gravity = 10;
+    public float maxVelocityChange = 10;
+    public float jumpHeight = 2;
+    public float points = 0;
+
+
+    private Rigidbody _rigidbody;
+    private bool grounded;
+
+    private bool dead; // true or fasle call
+    public int health; // player health 
+    private Transform PlayerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerTransform = GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
+        _rigidbody.freezeRotation = true;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerTransform.Rotate(0,(Mathf.Clamp( (Input.GetAxis("Horizontal")*5),-3,13)), 0);
+
+        Vector3 targetVelocity = new Vector3(0,0,Input.GetAxis("Vertical"));
+        targetVelocity = PlayerTransform.TransformDirection(targetVelocity);
+        targetVelocity = targetVelocity * speed;
+        Vector3 velocity = _rigidbody.velocity;
+        Vector3 velocityChange = targetVelocity - velocity;
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+
+        _rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+
+   
+        
+
+        if (Input.GetButton("Jump") && grounded == true)
+        {
+            _rigidbody.velocity = new Vector3(velocity.x, CalculateJump(), velocity.z);
+        }
+
+
+        _rigidbody.AddForce(new Vector3(0, -gravity * _rigidbody.mass, 0));
+        grounded = false;
+
+
+
+    }
+
+
+    float CalculateJump()
+    {
+        float jump = Mathf.Sqrt(2 * jumpHeight * gravity);
+        return jump;
+    }
+
+
+    void OnCollisionStay()
+    {
+        grounded = true;    
+
+
+    }
+
+    void OnTriggerEnter(Collider Tomek)
+    {
+        if (Tomek.tag == "Coin")
+        {
+
+            points = points + 5;
+            Destroy(Tomek.gameObject);
+        }
+    }
+
+}
