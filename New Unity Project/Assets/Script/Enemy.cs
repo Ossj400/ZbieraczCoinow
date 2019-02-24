44e72e8323ceebbd47ff2e01d8a43f774c93c9b3
Ossj400 <Ossj400@gmail.com>
2019-02-24 21:29:10 +0100

    koncowka tutka

--- New Unity Project/Assets/Script/Enemy.cs
+++ New Unity Project/Assets/Script/Enemy.cs
@@ -13,14 +13,25 @@ public class Enemy : MonoBehaviour
     public State curState;
     public float speed = .5f;
     public float goToDistance = 13;
-    public float attackDistance = 4;
+    public float attackDistance = 1;
     public Transform target;
     public string PlayerTag = "Player";
+    public float attackTimer = 2;
+    private float curTime;
+    private Player playerScript;
 
 
     // Start is called before the first frame update
     IEnumerator  Start()
     {
+        target = GameObject.FindGameObjectWithTag(PlayerTag).transform;
+        curTime = attackTimer;
+
+        if (target != null)
+        {
+            playerScript = target.GetComponent<Player>();
+        }
+
         while (true)
         {
             switch(curState)
@@ -55,13 +66,39 @@ public class Enemy : MonoBehaviour
     void GoTo()
     {
         print("Hi we are in goto");
+        transform.LookAt(target);
+        if (Vector3.Distance(target.position, transform.position) > attackDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            curState = State.ATTACK;
+        }
+
     }
 
 
     void Attack()
     {
 
+        transform.LookAt(target);
+        curTime = curTime - Time.deltaTime;
+            if(curTime < 0)
+        {
+            playerScript.health--; 
+            curTime = attackTimer;
+        }
+
+        if (Vector3.Distance(target.position, transform.position) > attackDistance)
+        {
+            curState = State.GOTO; 
+        }
+      
+
+
     }
+
 
 
 }
